31211223f6914f82b7f809d7966e1132e78d505f
Barry Kooij <git@barrykooij.nl>
2018-03-05 16:39:11 +0100

    added downloads block

--- disputor.php
+++ disputor.php
@@ -11,6 +11,39 @@ if($con->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+function getProductNameById($id) {
+  global $con;
+  $id = intval($id);
+  $res = $con->query("SELECT `post_title` FROM `wp_posts` WHERE `ID` = ".$id.";");
+  if($res->num_rows > 0) {
+    $o = $res->fetch_object();
+    return $o->post_title;
+  }
+  return "";
+}
+
+function getProductNameBySlug($slug) {
+  global $con;
+  $slug = $con->real_escape_string($slug);
+  $res = $con->query("SELECT `post_title` FROM `wp_posts` WHERE `post_name` = '".$slug."';");
+  if($res->num_rows > 0) {
+    $o = $res->fetch_object();
+    return $o->post_title;
+  }
+  return "";
+}
+
+function getOrderIPAddress($id) {
+  global $con;
+  $id = intval($id);
+  $res = $con->query("SELECT `meta_value` FROM `wp_postmeta` WHERE `post_id` =  ".$id." AND `meta_key` = '_customer_ip_address';");
+  if($res->num_rows > 0) {
+    $o = $res->fetch_object();
+    return $o->meta_value;
+  }
+  return "";
+}
+
 // vars
 $license = $con->real_escape_string($argv[2]);
 
@@ -31,6 +64,42 @@ if($res->num_rows > 0) {
   $html = str_ireplace( '%ACTIVATION_EMAIL%', $o->activation_email, $html );
   $html = str_ireplace( '%DATE_CREATED%', $dt_created->format('Y-m-d'), $html );
   $html = str_ireplace( '%DATE_EXPIRES%', $dt_expires->format('Y-m-d'), $html );
+  $html = str_ireplace( '%USER_IP%', getOrderIPAddress($o->order_id), $html );
+}
+
+// fetch downloads
+$res = $con->query("SELECT * FROM `wp_license_wp_download_log` WHERE license_key='".$license."' ORDER BY `date_downloaded` ASC;");
+if($res->num_rows > 0) {
+
+  // we found at least 1 activation, load block
+  $download_block = file_get_contents('html/downloads.html');
+  $download_row = file_get_contents('html/download_row.html');
+  $rows_html = "";
+
+  while($o = $res->fetch_object()) {
+
+    $dt = new \DateTime($o->date_downloaded);
+
+    // create row
+    $row_html = (string)$download_row;
+    $row_html = str_ireplace( '%PRODUCT%', getProductNameById($o->api_product_id), $row_html );
+    $row_html = str_ireplace( '%IP%', $o->user_ip_address, $row_html );
+    $row_html = str_ireplace( '%DATE%', $dt->format('Y-m-d'), $row_html );
+
+    // add row to full rows html
+    $rows_html .= $row_html;
+
+  }
+
+  // set rows in block
+  $download_block = str_ireplace( '%DOWNLOAD_ROWS%', $rows_html, $download_block );
+
+  // set whole block in html
+  $html = str_ireplace( '%DOWNLOADS%', $download_block, $html );
+
+}else {
+  // replace var with empty string if no activations found
+  $html = str_ireplace( '%ACTIVATIONS%', "", $html );
 }
 
 // fetch activations
@@ -48,7 +117,7 @@ if($res->num_rows > 0) {
 
     // create row
     $row_html = (string)$activation_row;
-    $row_html = str_ireplace( '%PRODUCT_ID%', $o->api_product_id, $row_html );
+    $row_html = str_ireplace( '%PRODUCT_ID%', getProductNameBySlug($o->api_product_id), $row_html );
     $row_html = str_ireplace( '%WEBSITE%', $o->instance, $row_html );
     $row_html = str_ireplace( '%DATE%', $dt->format('Y-m-d'), $row_html );
 
